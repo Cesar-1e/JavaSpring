22ef97eb916bbd7067e9db37e4e8650d31e639ec
CesarCalad833 <cesar.augusto9782@gmail.com>
2021-03-16 21:19:41 -0500

    Estructura inicial del proyecto

---
+++ src/main/java/com/cesar1e/market/CesarMarketApplication.java
@@ -0,0 +1,13 @@
+package com.cesar1e.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CesarMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CesarMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cesar1e/market/CesarMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cesar1e.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CesarMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
