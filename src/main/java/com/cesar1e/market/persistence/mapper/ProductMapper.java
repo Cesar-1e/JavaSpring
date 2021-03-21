9fa6311d3a341febf25dafc6b6e47950c820f267
Cesar Augusto <cachxd1@gmail.com>
2021-03-21 18:54:29 -0500

    4. Construyendo nuestra API

--- src/main/java/com/cesar1e/market/persistence/mapper/ProductMapper.java
+++ src/main/java/com/cesar1e/market/persistence/mapper/ProductMapper.java
@@ -9,12 +9,12 @@ import org.mapstruct.Mappings;
 
 import java.util.List;
 
-@Mapper(componentModel = "Spring", uses = {CategoryMapper.class})
+@Mapper(componentModel = "spring", uses = {CategoryMapper.class})
 public interface ProductMapper {
     @Mappings({
             @Mapping(source = "idProducto", target = "productId"),
             @Mapping(source = "nombre", target = "name"),
-            @Mapping(source = "idCategoria", target = "productId"),
+            @Mapping(source = "idCategoria", target = "categoryId"),
             @Mapping(source = "precioVenta", target = "price"),
             @Mapping(source = "cantidadStock", target = "stock"),
             @Mapping(source = "estado", target = "active"),
