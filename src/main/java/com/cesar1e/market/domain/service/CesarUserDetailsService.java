5586ee5a15ea4712b6bfc9a8380207da6094c650
Cesar Augusto <cachxd1@gmail.com>
2021-03-22 16:41:19 -0500

    6. Spring Security

---
+++ src/main/java/com/cesar1e/market/domain/service/CesarUserDetailsService.java
@@ -0,0 +1,17 @@
+package com.cesar1e.market.domain.service;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class CesarUserDetailsService implements UserDetailsService {
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return new User("cesar", "{noop}platzi", new ArrayList<>());
+    }
+}
